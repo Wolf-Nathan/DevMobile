2b51ed6adf8d2eba056da340e4adbfac9007c3d9
Wolf Nathan <nathan-w@hotmail.fr>
2020-05-03 17:36:55 +0200

    [SmsBomber] Make contact's recyclerView

--- SmsBomber/app/src/main/java/com/example/smsbomber/ContactFragment.java
+++ SmsBomber/app/src/main/java/com/example/smsbomber/ContactFragment.java
@@ -5,9 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ContactFragment extends Fragment {
+    private ArrayList<Contact> listContact = new ArrayList<Contact>();
+    private ContactListAdapter mAdapter = new ContactListAdapter(listContact);
+
     public ContactFragment() {
         // Required empty public constructor
     }
@@ -18,6 +26,19 @@ public class ContactFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false);
 
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //recycler
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // define an adapter
+        recyclerView.setAdapter(mAdapter);
     }
 
 }
---
+++ SmsBomber/app/src/main/java/com/example/smsbomber/ContactListAdapter.java
@@ -0,0 +1,54 @@
+package com.example.smsbomber;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ViewHolder>{
+    private ArrayList<Contact> listdata;
+
+    public ContactListAdapter(ArrayList<Contact> listdata) {
+        this.listdata = listdata;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.fragment_contact_items, parent, false);
+        return new ViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final Contact contact = listdata.get(position);
+        final ContactListAdapter adapter = this;
+        //holder.textView.setText(listdata.get(position).getName());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return listdata.size();
+    }
+
+    public void MyUpdater() {
+        this.notifyDataSetChanged();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public LinearLayout linearLayout;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.textView = (TextView) itemView.findViewById(R.id.fragment_contact_item);
+            linearLayout = (LinearLayout)itemView.findViewById(R.id.linearLayout);
+        }
+    }
+}
