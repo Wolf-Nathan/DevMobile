34673e7c29a0d4b51ffb77daa40ab86f74a5bd2d
Wolf Nathan <nathan-w@hotmail.fr>
2020-05-14 13:55:33 +0200

    [MovieRating] Make search design

--- MovieRating/Components/Search.js
+++ MovieRating/Components/Search.js
@@ -25,7 +25,7 @@ class Search extends React.Component {
         else {
             var resultResearch = [];
             moviesData.forEach(movie => {
-                if(movie.title.includes(text, 0)){
+                if(movie.title.toUpperCase().includes(text.toUpperCase(), 0)){
                     resultResearch.push(movie);
                 }
             });
@@ -35,22 +35,50 @@ class Search extends React.Component {
         }
     }
 
-    render() {
-        return(
-            <View style={styles.container}>
-                <TextInput placeholder={"Rechercher"} onChangeText={(text) => this.research(text)}/>
+    list() {
+        if (this.state.movies.length > 0 ) {
+            return(
                 <FlatList
                     data={this.state.movies}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <MovieItem movie={item} displayMovieDetails={this.displayMoviesDetails}/>}
                 />
+            )
+        }
+        else {
+            return(
+                <Text style={styles.noMovie}>Aucun film ne correspond à votre recherche !</Text>
+            )
+        }
+    }
+
+    render() {
+        return(
+            <View style={styles.container}>
+                <TextInput style={styles.textInput} placeholder={"Rechercher"} onChangeText={(text) => this.research(text)}/>
+                {this.list()}
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        marginTop: 20
+    },
+    textInput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 30,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5,
+        marginBottom: 25
+    },
+    noMovie: {
+        alignSelf: 'center'
+    }
 });
 
 export default Search;
