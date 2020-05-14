43fd15f6518dd5bee7dece83537cb0d4e936302a
Wolf Nathan <nathan-w@hotmail.fr>
2020-05-14 13:31:40 +0200

    [MovieRating] Make Search component

---
+++ MovieRating/Components/Search.js
@@ -0,0 +1,52 @@
+// Components/Search.js
+
+import React from 'react'
+import {Button, Text, View, TextInput, FlatList, StyleSheet} from "react-native";
+import moviesData from "../Data/moviesData";
+import MovieItem from "./MovieItem";
+
+class Search extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: []
+        }
+    }
+
+    research(text) {
+        if(text === "") {
+            this.setState({ movies: []});
+        }
+        else {
+            var resultResearch = [];
+            moviesData.forEach(movie => {
+                if(movie.title.includes(text, 0)){
+                    resultResearch.push(movie);
+                }
+            });
+            this.setState({
+                movies: resultResearch
+            });
+        }
+    }
+
+    render() {
+        return(
+            <View style={styles.container}>
+                <TextInput placeholder={"Rechercher"} onChangeText={(text) => this.research(text)}/>
+                <FlatList
+                    data={this.state.movies}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <MovieItem movie={item} displayMovieDetails={this.displayMoviesDetails}/>}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Search;
