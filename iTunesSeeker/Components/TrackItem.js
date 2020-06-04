225da9e239d6b06c39404dd9633a8d23e8fee615
Wolf Nathan <nathan-w@hotmail.fr>
2020-06-04 16:23:51 +0200

    [iTunesSeeker] Show album image

--- iTunesSeeker/Components/TrackItem.js
+++ iTunesSeeker/Components/TrackItem.js
@@ -4,12 +4,23 @@ import React from 'react'
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 
 class TrackItem extends React.Component {
+    imageShow(track){
+        if(track.artworkUrl100){
+            return(
+                <Image style={styles.image} source={{ uri: track.artworkUrl100}} />
+            )
+        }
+        else {
+            <Image style={styles.image} source={require('../assets/singer.jpg')} />
+        }
+    }
+
     render() {
         const track = this.props.track;
         //const displayMovieDetails = this.props.displayMovieDetails;
         return (
             <TouchableOpacity onPress={() => console.log('hey')}>
-                <Image source={{ uri: track.artworkUrl100}} />
+                {this.imageShow(track)}
                 <View>
                     <View>
                         <Text>{track.trackName}</Text>
@@ -29,7 +40,12 @@ class TrackItem extends React.Component {
 }
 
 const styles = StyleSheet.create({
-
+    image: {
+        width: 100,
+        height: 100,
+        margin: 5,
+        backgroundColor: '#000000',
+    }
 });
 
 export default TrackItem;
